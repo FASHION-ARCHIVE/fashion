7f9ba4de7ce46c315a212467f4d939d4d104d0e3
jiwon <83341978+jiione@users.noreply.github.com>
2024-04-20 00:50:18 +0900

    Feat#6팀리스트페이지&상단바 (#8)
    * 링크 기능 추가 및 반응형 웹 변경
    
    * 상단바 헤더
    
    * 팀리스트 페이지 확정

---
+++ src/components/TopCategory.vue
@@ -0,0 +1,94 @@
+<template>
+    <header>
+        <div class="menu">
+            <div class="menu2">
+                <div class="text-1">2024 KUAD GRADUATION FASHION SHOW</div>
+                <div class="top-menu">
+                    <div class="title">TITLE</div>
+                    <div class="nav-links">
+                        <router-link to="/coming-soon"><div class="top1">LOOK BOOK</div></router-link>
+                        <router-link to="/coming-soon"><div class="top2">RUNWAY</div></router-link>
+                        <router-link to="/coming-soon"><div class="top3">SHOW INFO</div></router-link>
+                        <router-link to="/coming-soon"><div class="top4">BEHIND</div></router-link>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name: 'mainHeader'
+    }
+</script>
+
+<style>
+.menu {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    height: 144px;
+    width: 100%;
+    background-color: whitesmoke;
+    font-family: "Custom", Helvetica;
+    z-index: 100;
+}
+
+.menu2{
+    display: flex;
+    width: 1130px;
+    flex-direction: column;
+
+}
+
+.top-menu{
+    display: flex;
+    flex-direction: row;
+    height: 104px;
+    align-items: center;
+    justify-content: space-between;
+
+}
+
+.text-1{
+    position: relative;
+    display: flex;
+    align-items: center;
+    height:40px;
+    font-size: 10px;
+}
+
+
+
+.title{
+    display: flex;
+    font-size: 44px;
+}
+
+
+
+.nav-links {
+    display: flex;
+    font-size: 14px;
+}
+
+.nav-links a {
+    text-decoration: none; /* 밑줄 제거 */
+    color: black; /* 글씨 색상 변경 */
+}
+
+.nav-links a:hover {
+    color: gold; /* 호버 시 글씨 색상 변경 */
+}
+
+.top1 {
+    margin-right: 20px;
+}
+.top2 {
+    margin-right: 20px; 
+}
+.top3 {
+    margin-right: 20px; 
+}
+</style>
---
+++ src/views/TeamListView.vue
@@ -0,0 +1,104 @@
+
+
+<template>
+  <mainHeader/>
+  <div class = "team-background">
+    <div class="team-list-container">
+      <div class="team-list-text">TEAM LIST</div>
+      <div class ="image-grid">
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 1" alt="team 1" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">BUG REPORT</div>
+        </div>
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 2" alt="team 2" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">ANACHRONIA</div>
+        </div>
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 3" alt="team 3" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">PROTOPHOBIA</div>
+        </div>
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 4" alt="team 4" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">팀 4</div>
+        </div>
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 5" alt="team 5" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">팀 5</div>
+        </div>
+        <div class="image-item">
+          <router-link to = "/coming-soon"><img class="team 6" alt="team 6" src="@/assets/images/yy.jpeg" /></router-link>
+          <div class="caption">팀 6</div>
+        </div>
+      </div>
+    </div>
+    
+    
+  </div> 
+</template>
+
+<script>
+    import mainHeader from '@/components/TopCategory.vue';
+    export default {
+       name: 'App',
+       components: {
+         mainHeader
+       }
+     }
+</script>
+
+<style>
+@font-face {
+  font-family:'Custom';
+  src: url('../assets/fonts/TitilliumWeb-Regular.ttf') format('truetype');
+  font-weight: 400;
+}
+.team-background{
+  /* 배경 이미지 적용 */
+  display: flex;
+  height: auto;
+  background-image: url('@/assets/images/01_KUAD_background.png'); 
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  justify-content: center;
+  margin-top: 144px;
+}
+
+.team-list-container{
+  width: 1130px;
+  display: flex;
+  flex-direction: column;
+  margin-top:30px;
+}
+.team-list-text {
+  display: flex;
+  position: relative;
+  font-size: 28px;
+  font-family: 'Custom'; /* 필요한 경우 폰트 스타일 지정 */
+}
+
+.image-grid {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.image-item img {
+  margin-top: 30px;
+  display: block;
+  width: 300px; /* 이미지의 너비 */
+  height: 300px; /* 이미지의 높이 */
+  object-fit: cover;
+  border: 4px solid whitesmoke;
+}
+
+
+.caption{
+  color: black;
+  margin-top: 10px;
+  text-align: center;
+}
+</style>
