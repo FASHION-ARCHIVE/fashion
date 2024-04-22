4a79ca7695ce19262d598718c7bfc1285fbc58cb
jayyeong <48705640+jayyeong@users.noreply.github.com>
2024-04-22 12:40:27 +0900

    init

---
+++ src/views/BehindView.vue
@@ -0,0 +1,14 @@
+<template>
+   <mainHeader/>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+    import mainHeader from '@/components/TopCategory.vue';
+
+    export default defineComponent({
+    components: {
+        mainHeader // 컴포넌트 등록
+    },
+})
+</script>
