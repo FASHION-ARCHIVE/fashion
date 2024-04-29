39193a303cfea403162331fcc4ffe330b8177ff2
jayyeong <48705640+jayyeong@users.noreply.github.com>
2024-04-30 03:11:52 +0900

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+      <div class="footer-content">
+        <p>This is a footer.</p>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'mainFooter'
+  }
+  </script>
+  
+  <style scoped>
+  footer {
+    background-color: #333;
+    color: white;
+    padding: 20px;
+  }
+  
+  .footer-content {
+    text-align: center;
+  }
+  </style>
