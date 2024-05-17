d21d486f0c02831eea2497684ce1737b2be08cb4
jiwon <pg15320@gmail.com>
2024-05-17 22:43:40 +0900

    create

--- src/views/teams/bugreport/bugreportLookbookView.vue
+++ src/views/teams/bugreport/bugreportLookbookView.vue
@@ -25,11 +25,65 @@ export default {
     return {
       singlePage: false,
       pages: [
-        "https://images.pexels.com/photos/4380970/pexels-photo-4380970.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-        "https://images.pexels.com/photos/2709385/pexels-photo-2709385.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "https://images.pexels.com/photos/2709385/pexels-photo-2709385.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "https://images.pexels.com/photos/3081752/pexels-photo-3081752.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "https://images.pexels.com/photos/3083250/pexels-photo-3083250.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        null,
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/000%E1%84%87%E1%85%A5%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%91%E1%85%A9%E1%84%90%E1%85%B3-%E1%84%91%E1%85%AD%E1%84%8C%E1%85%B5000.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_1.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_2.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_3.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_4.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_5.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_6.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_7.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_8.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_9.jpg",
+
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_10.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_11.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_12.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_13.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_14.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_15.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_16.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_17.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_18.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_19.jpg",
+
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_20.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_21.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_22.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_23.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_24.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_25.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_26.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_27.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_28.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_29.jpg",
+
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_30.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_31.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_32.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_33.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_34.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_35.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_36.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_37.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_38.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_39.jpg",
+
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_40.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_41.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_42.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_43.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_44.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_45.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_46.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_47.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_48.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_49.jpg",
+
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/1715948100326-a5024fd4-577c-4220-a02c-851a7e4bf603_50.jpg",
+        "https://d2d7g45pzqvt7g.cloudfront.net/bugreport/zzzzzzzzzzz뒷표지zzzzz-51.jpg"
+        
       ]
     };
   }
