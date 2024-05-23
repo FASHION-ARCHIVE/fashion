511649965091539de5cbf83c5d376b26094f761d
jiwon <83341978+jiione@users.noreply.github.com>
2024-05-23 23:55:50 +0900

    Update ShowinfoDesktop.vue (#91)

--- src/components/Showinfo/ShowinfoDesktop.vue
+++ src/components/Showinfo/ShowinfoDesktop.vue
@@ -19,7 +19,7 @@
                 </div>
                 <div class ="theme-info-container">
                     <div class="theme-name" data-aos="fade-left" data-aos-delay = 300 data-aos-once="false">PROTOTYPE</div>
-                    <div class="theme-info1" data-aos="fade-left" data-aos-delay = 300 data-aos-once="false">: The first impression as a <br>designer in society</div>
+                    <div class="theme-info1" data-aos="fade-left" data-aos-delay = 300 data-aos-once="false">: The first impression as a designer in society</div>
                     <div class="theme-info2"  data-aos="fade-left" data-aos-delay = 300 data-aos-once="false">프로토타입은 단순한 기본형을 넘어 모든 창조의 본질적 출발점입니다. 
                         <br><br>이는 우리 디자이너들이 사회에 첫 선을 보이는 작품이자, 자신을 대표하는 첫 인상이라 할 수 있습니다. 
                         '프로'는 지지, '투'는 대상에 대하여, '타입'은 형태를 의미합니다. 이를 통해 우리는 모든 형태를 지지한다는 의미를 담고 있습니다. 
