1b81a56e3a3424cd72597ad9072799196577080b
jiwon <83341978+jiione@users.noreply.github.com>
2024-05-23 23:28:16 +0900

    팀뷰글자사이즈 수정 (#89)

--- src/views/teams/ANACHRONIA/ANACHRONIAView.vue
+++ src/views/teams/ANACHRONIA/ANACHRONIAView.vue
@@ -207,7 +207,7 @@ export default defineComponent({
 
     .team-name{
         /* line-height: 0.7; */
-        font-size:64px;
+        font-size:55px;
         font-weight: 700;
     }
 
@@ -216,7 +216,7 @@ export default defineComponent({
     }
 
     .team-context{
-        font-size:20px;
+        font-size:18px;
         margin-top: 20px;
         margin-bottom: 30px;
     }
@@ -226,12 +226,12 @@ export default defineComponent({
 
     .team-info3{
         margin-top: 0px;
-        font-size:28px;
+        font-size:26px;
         font-weight: 700;
     }
 
     .team-info4{
-        font-size:20px;
+        font-size:18px;
     }
 
     .team-youtube-container{
--- src/views/teams/OAK/OAKView.vue
+++ src/views/teams/OAK/OAKView.vue
@@ -222,7 +222,7 @@ export default defineComponent({
 
     .team-name{
         /* line-height: 0.7; */
-        font-size:64px;
+        font-size:55px;
         font-weight: 700;
     }
 
@@ -231,7 +231,7 @@ export default defineComponent({
     }
 
     .team-context{
-        font-size:20px;
+        font-size:18px;
         margin-top: 20px;
         margin-bottom: 30px;
     }
@@ -241,12 +241,12 @@ export default defineComponent({
 
     .team-info3{
         margin-top: 0px;
-        font-size:28px;
+        font-size:26px;
         font-weight: 700;
     }
 
     .team-info4{
-        font-size:20px;
+        font-size:18px;
     }
 
     .team-youtube-container{
