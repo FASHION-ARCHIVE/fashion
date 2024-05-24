5cb8a963bb8e46811f937f8529cd97255743062e
jiwon <pg15320@gmail.com>
2024-05-24 21:02:21 +0900

    룩북 커서 모양 변경

--- src/views/teams/bugreport/bugreportLookbookView.vue
+++ src/views/teams/bugreport/bugreportLookbookView.vue
@@ -131,10 +131,12 @@ export default {
   align-items: center;
 }
 .left-icon{
+  cursor: pointer;
   margin-left: 50px;
   width: 30px;
 }
 .right-icon{
+  cursor: pointer;
   margin-right: 50px;
   width: 30px;
 }
